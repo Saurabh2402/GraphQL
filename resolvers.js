03f4eb7014bb97e07634aaf1c81dd52681877eeb
Saurabh <saurabh.yelmame19@vit.edu>
2023-10-16 01:02:50 +0530

    addgame, updateGame, deleteGame Mutations added

--- resolvers.js
+++ resolvers.js
@@ -49,4 +49,39 @@ export const resolvers = {
       return _db.authors.find((a) => a.id === parent.author_id);
     },
   },
+
+  Mutation: {
+    deleteGame(_, args) {
+      return (_db.games = _db.games.filter((g) => g.id != args.id));
+    },
+
+    addGame(_, args) {
+      console.log("args::", args);
+      const newGame = {
+        ...args.inp,
+        id: Math.floor(Math.random() * 10000).toString(),
+      };
+      _db.games.push(newGame);
+      return newGame;
+    },
+
+    updateGame(_, args) {
+      console.log("args::", args);
+      const updatedProperties = args.inp;
+      for (let i = 0; i < _db.games.length; i++) {
+        if (args.id === _db.games[i].id) {
+          console.log("Game found at index: ", i);
+          for (let property in updatedProperties) {
+            if (property in _db.games[i]) {
+              _db.games[i][property] = updatedProperties[property];
+            }
+          }
+          return _db.games[i];
+        } else {
+          console.log("Game not found, id of game: ", args.id);
+        }
+      }
+      return null;
+    },
+  },
 };
